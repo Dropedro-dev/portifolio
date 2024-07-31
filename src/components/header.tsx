a039aa8046c39117c013e85284f512de75be9c8a
Pedro Paulo Cunha <pedropaulocunha.dev@gmail.com>
2024-07-30 21:54:26 -0300

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+const Header =()=> {
+    return (
+        <header className="container mx-auto max-w-2xl flex items-center justify-between py-4">
+            <div className="flex-1 flex">
+                <div>menu</div>
+            </div>
+            <div className="flex-1 flex justify-center">
+                <h2 className="text-2xl font-bold">Portifólio</h2>
+            </div>
+            <div className="flex-1 flex justify-end">
+                <div>network</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
