7c14bf2f7d8d38400135b91ae82e14537e880f92
Pedro Paulo Cunha <pedropaulocunha.dev@gmail.com>
2024-08-02 14:31:29 -0300

    refatoração do layout do componente Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,18 @@
+import { IoMenu } from "react-icons/io5";
+import { FaGithubSquare,FaLinkedin } from "react-icons/fa";
+
+
+
 const Header =()=> {
     return (
-        <header className="container mx-auto max-w-2xl flex items-center justify-between py-4">
-            <div className="flex-1 flex">
-                <div>menu</div>
-            </div>
-            <div className="flex-1 flex justify-center">
+        <header className="container flex">
+            <div className="flex flex-1 mx-auto items-center justify-between max-w-2xl py-4">
+                <div className="text-2xl cursor-pointer ml-3"><IoMenu /></div>
                 <h2 className="text-2xl font-bold">Portifólio</h2>
-            </div>
-            <div className="flex-1 flex justify-end">
-                <div>network</div>
+                <ul className="flex gap-3 text-xl items-center mr-3">
+                    <li><a href=""><FaGithubSquare /></a></li>
+                    <li><a href=""><FaLinkedin /></a></li>
+                </ul>
             </div>
         </header>
     )
