dd44330db4b09f11f3bbac1c45a541c65dbadf93
Pedro Paulo Cunha <pedropaulocunha.dev@gmail.com>
2024-11-12 15:11:41 -0300

    inicializando aboutSection

--- src/components/aboutSection/aboutSection.tsx
+++ src/components/aboutSection/aboutSection.tsx
@@ -2,9 +2,17 @@ export const AboutSection = () => {
     return (
         <section 
             id="about"
-            className="w-screen h-screen"
+            className="max-w-4xl mx-auto flex bg-red-500"
         >
-            About Section
+            <div className="py-10 flex-1 flex flex-col lg:flex-row-reverse bg-blue-500">
+                <div className="flex-1 flex justify-center items-center bg-green-500">
+                    <div className="relative h-36 w-24 bg-red-500">
+                        <div className="absolut h-36 w-24 bg-black -left-3 bo"></div>
+                    </div>
+                </div>
+                <div className="flex-1 bg-yellow-500">side</div>
+
+            </div>
         </section>
     )
 }
